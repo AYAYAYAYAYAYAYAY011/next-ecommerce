b8ad7e1ec39f854d6dfd3b0ae2854e61ade5ebb3
AYAYAYAYAYAYAYAY011 <werachai91148@gmail.com>
2024-10-22 15:39:35 +0700

    back commit

--- src/app/(main-website)/page.tsx
+++ src/app/(main-website)/page.tsx
@@ -2,10 +2,19 @@ import Link from "next/link";
 import AppLogo from "./components/AppLogo";
 import { Container } from "@mui/material";
 
-export default function Home(){
+async function getData(){
+  const response = await fetch('https://dummyjson.com/users');
+  if(!response.ok){
+    throw new Error("ไม่สามารถ Fetch Data ได้");
+
+  }
+  return await response.json();
+}
+export default async function Home(){
+  const response = await getData();
   return(
     <Container>
-      <h1>Home page</h1>
+      <h1>{JSON.stringify(response.users)}</h1>
     <AppLogo title="Logo One" c="red"/>
     <AppLogo title="Logo Two" c="blue"/><br/>
     <Link href="../content/resume">Resume</Link><br/>
