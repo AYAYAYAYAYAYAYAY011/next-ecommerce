da7bf0e6585c28cc9a8b10a2e5b41b445385e8bb
AYAYAYAYAYAYAYAY011 <werachai91148@gmail.com>
2024-10-22 15:10:49 +0700

    Back commit

--- src/app/(main-admin)/dashboard/page.tsx
+++ src/app/(main-admin)/dashboard/page.tsx
@@ -1,12 +1,13 @@
+import { Container } from "@mui/material";
 import Link from "next/link";
 
 export default function DashboardPage() {
   return (
-    <main>
+    <Container>
       <h1>Admin Dashboard Page</h1>
       <Link href="../content/resume">Resume</Link><br/>
         <Link href="../">Main Page</Link><br/>
         <Link href="../abount">Go to About</Link>
-    </main>
+    </Container>
   );
 }
