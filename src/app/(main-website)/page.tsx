ae11f80c427eed2d452c6326e38df932c0145dc5
AYAYAYAYAYAYAYAY011 <werachai91148@gmail.com>
2024-11-19 14:21:12 +0700

    re01

--- src/app/(main-website)/page.tsx
+++ src/app/(main-website)/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import AppLogo from "./components/AppLogo";
-import { Container } from "@mui/material";
+import { Container, Divider, Grid, Paper, Typography } from "@mui/material";
+import Image from "next/image";
 
 async function getData(){
-  const response = await fetch('https://dummyjson.com/users');
+  const response = await fetch('https://dummyjson.com/products',{cache: `no-store`});
   if(!response.ok){
     throw new Error("ไม่สามารถ Fetch Data ได้");
 
@@ -14,7 +15,28 @@ export default async function Home(){
   const response = await getData();
   return(
     <Container>
-      <h1>{JSON.stringify(response.users)}</h1>
+      <h1>Products</h1>
+      {
+        response.products &&(
+          <>
+          <Grid>
+            {
+              response.products.map((item: any) =>{
+              return(<Grid item key={item.id} lg={3} xs={6}>
+              <Paper>
+                <Image src={item.thumbnail} alt={item.detail} />
+                <Divider></Divider>
+                <Typography>{item.detail}</Typography>
+              </Paper>
+              </Grid>)
+
+            })
+          }
+          </Grid>
+          </>
+        )
+      }
+      <h1>{JSON.stringify(response.products)}</h1>
     <AppLogo title="Logo One" c="red"/>
     <AppLogo title="Logo Two" c="blue"/><br/>
     <Link href="../content/resume">Resume</Link><br/>
